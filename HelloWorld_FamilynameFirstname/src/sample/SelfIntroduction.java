0830af91f8459edc4b484cbcea5042425fd8e200
TABLET-S4F2G6PV\ryoni <ryo.nishida1993@gmail.com>
2023-07-25 23:22:58 +0900

    課題１＿３

---
+++ HelloWorld_FamilynameFirstname/src/sample/SelfIntroduction.java
@@ -0,0 +1,24 @@
+package sample;
+
+public class SelfIntroduction {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		// 氏名
+		String name = "西田　遼";
+		// 年齢
+		int age = 30;
+		// 趣味
+		String hobby = "LIVE";
+
+
+		/*
+		* テスト
+		*/
+
+		System.out.println("私の名前は" + name + "です。");
+		System.out.println("年齢は" + age + "歳です。");
+		System.out.println("趣味は" + hobby + "です。");
+	}
+
+}
